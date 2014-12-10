fe2ef0985c2f528b3ab2d1799f200c1d86b4ff7f
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-10 13:08:50 -0800

    Allow custom file name when dumping recording

--- lib/yetty/record.rb
+++ lib/yetty/record.rb
@@ -2,6 +2,38 @@ require 'yetty'
 require 'shelr'
 
 module Yetty
+
+  class Publisher < Shelr::Publisher
+
+    attr_reader :file_name
+
+    def initialize(file_name='yetty.json')
+      @file_name = file_name
+      super()
+    end
+
+    def prepare(id)
+      out = {}
+      ['meta', 'timing', 'typescript'].each do |file|
+        out[file] = File.read(File.join(Shelr.data_dir(id), file))
+      end
+
+      meta = JSON.parse(out.delete('meta'))
+      meta.each { |k,v| out[k] = v }
+      STDOUT.print 'Description: '
+      out['description'] = STDIN.gets.strip
+      STDOUT.print 'Tags (ex: howto, linux): '
+      out['tags'] = STDIN.gets.strip
+      out['private'] = @private
+      return out.to_json
+    end
+
+    def dump_filename
+      file_name
+    end
+
+  end
+
   class Recorder < Shelr::Recorder
 
     def record!(options={})
@@ -34,7 +66,7 @@ module Yetty
     # Invoke shelr record
     def execute!
       Yetty::Recorder.record!(opts)
-      Shelr::Publisher.new.dump('last')
+      Yetty::Publisher.new(arguments.first || 'yetty.json').dump('last')
     end
 
   end
